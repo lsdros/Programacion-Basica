6a87e418dd31a4ced679a6d27b69932e04bccd65
lap lsdros <luiscarloshernandezgalicia6@gmail.com>
2025-01-25 22:00:44 -0600

    Create Laboratorio Semana 1.py

---
+++ Semana 1/Laboratorio Semana 1.py
@@ -0,0 +1,90 @@
+Ejercicio 1
+
+ ¿Cuál es la función de la memoria secundaria en una computadora?
+ a) Ejecutar todos los cálculos y la logica del programa.
+ b) Desvargar páginas web de Internet
+ c) Almacenar información durante mucho tiempo, incluso después de ciclos de apagados y encendido
+ d) Recolectar la entrada del usuario
+
+Respuesta: c)
+
+Ejercicio 2
+
+ ¿Que es un programa?
+
+Respuesta: Es la secuencia de declaraciones o sentencias que han sido diseñadas para hacer algo
+
+Ejercicio 3
+
+ ¿Cuál es la diferencia entre un compilador y un intérpete?
+
+Respuesta : Un compilador requiere de un programa completo en un fichero, para despues traducirlo a codigo maquina y de esta manera ponerlo dentro de otro fichero para su ejecucion, mientras que la esencia de un interpete consiste en ser capaz de mantener una conversacion interactiva 
+
+Ejercicio 4
+ 
+ ¿Cuál de los siguientes contiene "codigo Máquina"?
+ a) El interprete de Python
+ b) El teclado
+ c) El código fuente de Python
+ d) Un documento de un procesador de texto
+
+Respuesta: a)
+
+Ejercicio 5
+ 
+ ¿Que esta mal en el siguiente codigo?
+
+ >>> primt '¡Hola, mundo!'
+ File "<stdin>", line 1
+ primt 'Hola, mundo!'
+
+ SyntaxError: invalid syntax
+ >>>
+
+ Respuesta: Como la palabra print esta mal escrita el la palabra como no esta reservada dara syntax error en vez de imprimir la palabra 
+
+Ejercicio 6
+
+ ¿En qué lugart del computador queda almacenada una variable, como en este caso "x", después de ejecutar la siguiente línea de Pyhton?
+ 
+ X = 123
+ a) Unidad central de procesamiento
+ b) Memoria Principal
+ c) Memoria Secundaria
+ d) Dispositivos de Entrada
+ e) Dispositivos de Salida
+
+Respuesta : b)
+
+Ejercicio 7
+
+ ¿Qué mostrará en pantalla el siguiente programa?
+
+ x = 43
+ x = x + 1
+ print (x)
+ a) 43
+ b) 44
+ c) x + 1
+ d) Error, porque x = x + 1 no es posible matematicamente
+
+Respuesta : b)
+
+Ejercicio 8
+ 
+ Explica cada uno de los siguientes conceptos usando un ejemplo de una capacidad humana: 
+ 1. Unidad central del prosecamiento : este funciona al igual que un corazon humano, dandole vida a la computadora
+
+ 2. Memoria principal : puede explicarse como los ojos, una vez que estos se cierren pierdes la vista hasta que los vuelvas a abrir, con la memoria principal puede ser lo mismo al apagar la computadora y perdera todo hasta que la prendes otra vez y empezara a recopilar nueva informacion
+
+ 3. Memoria secundaria : El cerebro, este recopila informacion siempre y lo tendremos siempre con nosotros
+
+ 4. Dispositivos de entrada : Es como el oido este recopila informacion que se le de
+
+ 5. Dispositivos de salida : Es como la boca este dara el resultado a base de lo que recopilo con los dispositivos de entrada
+
+Ejercicio 9
+ 
+ ¿Cómo puedes corregir un "Error de sintaxis"?
+ 
+ Respuesta : Comprobar que el codigo que hayas escrito este bien gramaticalmente y que las palabras reservadas de Python esten bien escritas y usadas correctamente
